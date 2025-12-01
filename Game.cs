7105af52fab9674db44f54764e8822994e0e1793
Marcos Ian <eoquema7uco@gmail.com>
2025-12-01 15:18:07 -0500

    tiles and background

--- Game.cs
+++ Game.cs
@@ -10,16 +10,16 @@ namespace knight;
 
 public sealed class Game : GameWindow
 {
-  private readonly string _contentRoot;
+    private readonly string _contentRoot;
 
-  private Shader? _shader;
-  private Camera? _camera;
-  private SceneManager? _sceneManager;
-  private GameScene? _gameScene;
+    private Shader? _shader;
+    private Camera? _camera;
+    private SceneManager? _sceneManager;
+    private GameScene? _gameScene;
 
-  private Matrix4 _projection;
+    private Matrix4 _projection;
 
-  private const string VertexShaderSource = @"
+    private const string VertexShaderSource = @"
 #version 330 core
 layout(location = 0) in vec2 aPosition;
 layout(location = 1) in vec2 aTexCoord;
@@ -36,7 +36,7 @@ void main()
     vTexCoord = aTexCoord;
 }";
 
-  private const string FragmentShaderSource = @"
+    private const string FragmentShaderSource = @"
 #version 330 core
 in vec2 vTexCoord;
 
@@ -49,126 +49,127 @@ void main()
     FragColor = texture(uTexture, vTexCoord);
 }";
 
-  public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings, string? contentRoot = null)
-      : base(gameWindowSettings, nativeWindowSettings)
-  {
-    _contentRoot = contentRoot ?? AppContext.BaseDirectory;
-  }
-
-  protected override void OnLoad()
-  {
-    base.OnLoad();
-
-    GL.ClearColor(0.1f, 0.12f, 0.16f, 1f);
-    GL.Enable(EnableCap.Blend);
-    GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
-    GL.Viewport(0, 0, FramebufferSize.X, FramebufferSize.Y);
-
-    _shader = new Shader(VertexShaderSource, FragmentShaderSource);
-    _shader.Use();
-    _shader.SetInt("uTexture", 0);
-
-    UpdateProjection();
-    _shader.SetMatrix4("uProjection", _projection);
-
-    _camera = new Camera(new Vector2(ClientSize.X / 2f, ClientSize.Y / 2f), new Vector2(ClientSize.X, ClientSize.Y));
-
-    _sceneManager = new SceneManager(_contentRoot, ClientSize);
-    _gameScene = new GameScene();
-    _sceneManager.LoadScene(_gameScene);
-  }
-
-  protected override void OnUpdateFrame(FrameEventArgs args)
-  {
-    base.OnUpdateFrame(args);
-
-    if (_sceneManager is null || _camera is null)
+    public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings, string? contentRoot = null)
+        : base(gameWindowSettings, nativeWindowSettings)
     {
-      return;
+        _contentRoot = contentRoot ?? AppContext.BaseDirectory;
     }
 
-    // Pass keyboard state to game scene
-    if (_gameScene is not null)
+    protected override void OnLoad()
     {
-      _gameScene.SetKeyboardState(KeyboardState);
+        base.OnLoad();
+
+        GL.ClearColor(0.1f, 0.12f, 0.16f, 1f);
+        GL.Enable(EnableCap.Blend);
+        GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+        GL.Viewport(0, 0, FramebufferSize.X, FramebufferSize.Y);
+
+        _shader = new Shader(VertexShaderSource, FragmentShaderSource);
+        _shader.Use();
+        _shader.SetInt("uTexture", 0);
+
+        UpdateProjection();
+        _shader.SetMatrix4("uProjection", _projection);
+
+        _camera = new Camera(new Vector2(ClientSize.X / 2f, ClientSize.Y / 2f), new Vector2(ClientSize.X, ClientSize.Y));
+
+        _sceneManager = new SceneManager(_contentRoot, ClientSize);
+        _gameScene = new GameScene();
+        _sceneManager.LoadScene(_gameScene);
     }
 
-    _sceneManager.Update(args.Time);
-
-    // Update camera to follow player
-    if (_gameScene?.Player is not null)
+    protected override void OnUpdateFrame(FrameEventArgs args)
     {
-      _camera.FollowTarget(_gameScene.Player.Position, smoothing: 0.1f);
-    }
-  }
+        base.OnUpdateFrame(args);
 
-  protected override void OnRenderFrame(FrameEventArgs args)
-  {
-    base.OnRenderFrame(args);
+        if (_sceneManager is null || _camera is null)
+        {
+            return;
+        }
 
-    GL.Clear(ClearBufferMask.ColorBufferBit);
+        // Pass keyboard state to game scene
+        if (_gameScene is not null)
+        {
+            _gameScene.SetKeyboardState(KeyboardState);
+        }
 
-    if (_shader is null || _camera is null || _sceneManager is null)
-    {
-      SwapBuffers();
-      return;
-    }
+        _sceneManager.Update(args.Time);
 
-    _sceneManager.Draw(_shader, _camera);
-
-    SwapBuffers();
-  }
-
-  protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
-  {
-    base.OnFramebufferResize(e);
-
-    GL.Viewport(0, 0, e.Width, e.Height);
-    UpdateProjection();
-
-    if (_shader is not null)
-    {
-      _shader.Use();
-      _shader.SetMatrix4("uProjection", _projection);
+        // REMOVED: Camera follow code
+        // The camera now stays fixed at its initial position
+        // if (_gameScene?.Player is not null)
+        // {
+        //   _camera.FollowTarget(_gameScene.Player.Position, smoothing: 0.1f);
+        // }
     }
 
-    if (_camera is not null)
+    protected override void OnRenderFrame(FrameEventArgs args)
     {
-      _camera.ViewportSize = new Vector2(e.Width, e.Height);
+        base.OnRenderFrame(args);
+
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        if (_shader is null || _camera is null || _sceneManager is null)
+        {
+            SwapBuffers();
+            return;
+        }
+
+        _sceneManager.Draw(_shader, _camera);
+
+        SwapBuffers();
     }
 
-    _sceneManager?.OnResize(new Vector2i(e.Width, e.Height));
-  }
-
-  protected override void OnResize(ResizeEventArgs e)
-  {
-    base.OnResize(e);
-
-    UpdateProjection();
-
-    if (_shader is not null)
+    protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
     {
-      _shader.Use();
-      _shader.SetMatrix4("uProjection", _projection);
+        base.OnFramebufferResize(e);
+
+        GL.Viewport(0, 0, e.Width, e.Height);
+        UpdateProjection();
+
+        if (_shader is not null)
+        {
+            _shader.Use();
+            _shader.SetMatrix4("uProjection", _projection);
+        }
+
+        if (_camera is not null)
+        {
+            _camera.ViewportSize = new Vector2(e.Width, e.Height);
+        }
+
+        _sceneManager?.OnResize(new Vector2i(e.Width, e.Height));
     }
 
-    if (_camera is not null)
+    protected override void OnResize(ResizeEventArgs e)
     {
-      _camera.ViewportSize = new Vector2(e.Width, e.Height);
+        base.OnResize(e);
+
+        UpdateProjection();
+
+        if (_shader is not null)
+        {
+            _shader.Use();
+            _shader.SetMatrix4("uProjection", _projection);
+        }
+
+        if (_camera is not null)
+        {
+            _camera.ViewportSize = new Vector2(e.Width, e.Height);
+        }
+
+        _sceneManager?.OnResize(new Vector2i(e.Width, e.Height));
     }
 
-    _sceneManager?.OnResize(new Vector2i(e.Width, e.Height));
-  }
+    protected override void OnUnload()
+    {
+        _sceneManager?.Unload();
+        _shader?.Dispose();
+        base.OnUnload();
+    }
 
-  protected override void OnUnload()
-  {
-    _sceneManager?.Unload();
-    _shader?.Dispose();
-    base.OnUnload();
-  }
-
-  private void UpdateProjection()
-  {
-    _projection = Matrix4.CreateOrthographicOffCenter(0, ClientSize.X, 0, ClientSize.Y, -1f, 1f);
-  }
+    private void UpdateProjection()
+    {
+        _projection = Matrix4.CreateOrthographicOffCenter(0, ClientSize.X, 0, ClientSize.Y, -1f, 1f);
+    }
 }
--- Scenes/GameScene.cs
+++ Scenes/GameScene.cs
@@ -11,163 +11,382 @@ namespace knight.Scenes;
 
 public sealed class GameScene : IScene
 {
-  private Player? _player;
-  private readonly List<GroundTile> _groundTiles = new();
-  private string? _groundTexturePath;
-  private Vector2i _groundTextureSize;
-  private Vector2i _viewportSize;
-  private KeyboardState? _keyboard;
+    private Player? _player;
+    private readonly List<GroundTile> _platforms = new();
+    private string? _groundTexturePath;
+    private Vector2i _groundTextureSize;
 
-  private const double MaxDeltaSeconds = 1d / 60d;
+    // Four different platform textures
+    private string? _platform1TexturePath;
+    private Vector2i _platform1TextureSize;
+    private string? _platform2TexturePath;
+    private Vector2i _platform2TextureSize;
+    private string? _platform3TexturePath;
+    private Vector2i _platform3TextureSize;
+    private string? _platform4TexturePath;
+    private Vector2i _platform4TextureSize;
 
-  public void SetKeyboardState(KeyboardState keyboard)
-  {
-    _keyboard = keyboard;
-  }
+    private Vector2i _viewportSize;
+    private KeyboardState? _keyboard;
 
-  public void Initialize(string contentRoot, Vector2i viewportSize)
-  {
-    _viewportSize = viewportSize;
+    // Background fields
+    private SpriteRenderer? _backgroundRenderer;
+    private GroundTile? _background;
 
-    // Load ground texture
-    var groundSpritePath = AssetHelper.RequireAsset(AssetHelper.GetAssetPath(contentRoot, "ground.png"), "Ground texture is missing.");
-    _groundTexturePath = groundSpritePath;
-    _groundTextureSize = AssetHelper.GetTextureSize(groundSpritePath);
-    
-    // Create player (animations loaded in constructor)
-    var playerScale = 2f;
-    var groundHeight = _groundTextureSize.Y;
-    var playerHeight = 40 * playerScale;
-    var playerStartY = groundHeight + playerHeight / 2f;
-    var playerStart = new Vector2(_viewportSize.X / 2f, playerStartY);
-    
-    _player = new Player(playerStart, contentRoot, playerScale);
+    private const double MaxDeltaSeconds = 1d / 60d;
 
-    BuildGroundTiles();
-  }
-
-  public void Update(double deltaSeconds)
-  {
-    if (_player is null)
+    public void SetKeyboardState(KeyboardState keyboard)
     {
-      return;
+        _keyboard = keyboard;
     }
 
-    var dt = Math.Min(deltaSeconds, MaxDeltaSeconds);
-
-    if (_keyboard is not null)
+    public void Initialize(string contentRoot, Vector2i viewportSize)
     {
-      InputSystem.HandlePlayerInput(_player, _keyboard);
-      InputSystem.UpdatePlayerAnimation(_player, _keyboard);
+        _viewportSize = viewportSize;
+
+        // Load background texture
+        var backgroundPath = AssetHelper.GetAssetPath(contentRoot, "background.png");
+        if (System.IO.File.Exists(backgroundPath))
+        {
+            var bgSize = AssetHelper.GetTextureSize(backgroundPath);
+            _backgroundRenderer = new SpriteRenderer();
+            _backgroundRenderer.LoadAnimation("Static", backgroundPath, bgSize.X, bgSize.Y, frameDurationSeconds: 0, loop: true);
+            _backgroundRenderer.SetAnimation("Static", true);
+
+            var bgPosition = new Vector2(viewportSize.X / 2f, viewportSize.Y / 2f);
+            _background = new GroundTile(bgPosition, _backgroundRenderer);
+
+            var scaleX = (float)viewportSize.X / bgSize.X;
+            var scaleY = (float)viewportSize.Y / bgSize.Y;
+            var scale = Math.Max(scaleX, scaleY);
+            _background.setScale(scale);
+        }
+
+        // Load ground texture (for the bottom floor)
+        var groundSpritePath = AssetHelper.RequireAsset(AssetHelper.GetAssetPath(contentRoot, "ground.png"), "Ground texture is missing.");
+        _groundTexturePath = groundSpritePath;
+        _groundTextureSize = AssetHelper.GetTextureSize(groundSpritePath);
+
+        // Load platform textures (for floating platforms)
+        // You can name these files whatever you want: platform1.png, platform2.png, etc.
+        LoadPlatformTexture(contentRoot, "platform1.png", ref _platform1TexturePath, ref _platform1TextureSize);
+        LoadPlatformTexture(contentRoot, "platform2.png", ref _platform2TexturePath, ref _platform2TextureSize);
+        LoadPlatformTexture(contentRoot, "platform3.png", ref _platform3TexturePath, ref _platform3TextureSize);
+        LoadPlatformTexture(contentRoot, "platform4.png", ref _platform4TexturePath, ref _platform4TextureSize);
+
+        // Create player
+        var playerScale = 2f;
+        var groundHeight = _groundTextureSize.Y;
+        var playerHeight = 40 * playerScale;
+        var playerStartY = groundHeight + playerHeight / 2f;
+        var playerStart = new Vector2(_viewportSize.X / 2f, playerStartY);
+
+        _player = new Player(playerStart, contentRoot, playerScale);
+
+        BuildLevel();
     }
 
-    PhysicsSystem.IntegrateVelocity(_player, dt);
-    PhysicsSystem.ClampToHorizontalBounds(_player, 0, _viewportSize.X);
-    PhysicsSystem.ResolveGroundCollisions(_player, _groundTiles);
-    
-    _player.Update(dt);
-
-    foreach (var tile in _groundTiles)
+    public void Update(double deltaSeconds)
     {
-      tile.Update(dt);
-    }
-  }
+        if (_player is null)
+        {
+            return;
+        }
 
-  public void Draw(Shader shader, Camera camera)
-  {
-    if (_player is null || shader is null)
-    {
-      return;
-    }
+        var dt = Math.Min(deltaSeconds, MaxDeltaSeconds);
 
-    shader.Use();
-    shader.SetMatrix4("uView", camera.ViewMatrix);
+        if (_keyboard is not null)
+        {
+            InputSystem.HandlePlayerInput(_player, _keyboard);
+            InputSystem.UpdatePlayerAnimation(_player, _keyboard);
+        }
 
-    foreach (var tile in _groundTiles)
-    {
-      tile.Draw(shader);
-    }
+        PhysicsSystem.IntegrateVelocity(_player, dt);
+        PhysicsSystem.ClampToHorizontalBounds(_player, 0, _viewportSize.X);
+        PhysicsSystem.ResolveGroundCollisions(_player, _platforms);
 
-    _player.Draw(shader);
-  }
+        _player.Update(dt);
 
-  public void OnResize(Vector2i newSize)
-  {
-    _viewportSize = newSize;
+        foreach (var platform in _platforms)
+        {
+            platform.Update(dt);
+        }
 
-    if (_player is not null)
-    {
-      var groundHeight = _groundTextureSize.Y;
-      var playerHeight = _player.Size.Y;
-      var playerY = groundHeight + playerHeight / 2f;
-      _player.Position = new Vector2(newSize.X / 2f, playerY);
+        _background?.Update(dt);
     }
 
-    BuildGroundTiles();
-  }
-
-  public void Unload()
-  {
-    _player?.Dispose();
-
-    foreach (var tile in _groundTiles)
+    public void Draw(Shader shader, Camera camera)
     {
-      tile.Dispose();
+        if (_player is null || shader is null)
+        {
+            return;
+        }
+
+        shader.Use();
+        shader.SetMatrix4("uView", camera.ViewMatrix);
+
+        // Draw background first (behind everything)
+        _background?.Draw(shader);
+
+        // Draw platforms
+        foreach (var platform in _platforms)
+        {
+            platform.Draw(shader);
+        }
+
+        // Draw player last (in front)
+        _player.Draw(shader);
     }
 
-    _groundTiles.Clear();
-  }
-
-  public Player? Player => _player;
-
-  private void BuildGroundTiles()
-  {
-    foreach (var tile in _groundTiles)
+    public void OnResize(Vector2i newSize)
     {
-      tile.Dispose();
-    }
-    _groundTiles.Clear();
+        _viewportSize = newSize;
 
-    if (_groundTexturePath is null || _groundTextureSize == Vector2i.Zero)
-    {
-      return;
-    }
+        if (_player is not null)
+        {
+            var groundHeight = _groundTextureSize.Y;
+            var playerHeight = _player.Size.Y;
+            var playerY = groundHeight + playerHeight / 2f;
+            _player.Position = new Vector2(newSize.X / 2f, playerY);
+        }
 
-    var tileWidth = _groundTextureSize.X;
-    var tileHeight = _groundTextureSize.Y;
+        // Update background size and position on resize
+        if (_background is not null)
+        {
+            var bgTexSize = AssetHelper.GetTextureSize(AssetHelper.GetAssetPath("", "background.png"));
+            if (bgTexSize != Vector2i.Zero)
+            {
+                var scaleX = (float)newSize.X / bgTexSize.X;
+                var scaleY = (float)newSize.Y / bgTexSize.Y;
+                var scale = Math.Max(scaleX, scaleY);
+                _background.setScale(scale);
+            }
+            _background.Position = new Vector2(newSize.X / 2f, newSize.Y / 2f);
+        }
 
-    if (tileWidth <= 0 || tileHeight <= 0)
-    {
-      return;
-    }
-
-    var tilesNeeded = (int)Math.Ceiling(_viewportSize.X / (float)tileWidth) + 1;
-
-    for (var i = 0; i < tilesNeeded; i++)
-    {
-      var renderer = new SpriteRenderer();
-      renderer.LoadAnimation("Static", _groundTexturePath, tileWidth, tileHeight, frameDurationSeconds: 0, loop: true);
-      renderer.SetAnimation("Static", true);
-
-      var x = i * tileWidth + tileWidth / 2f;
-      var position = new Vector2(x, tileHeight / 2f);
-      var tile = new GroundTile(position, renderer);
-      _groundTiles.Add(tile);
+        BuildLevel();
     }
 
-    // mountain
-    for (var i = 0; i < 4; i++) 
+    public void Unload()
     {
-      var renderer = new SpriteRenderer();
-      renderer.LoadAnimation("Static", _groundTexturePath, tileWidth, tileHeight, frameDurationSeconds: 0, loop: true);
-      renderer.SetAnimation("Static", true);
-      var y = (3.2f/2 + i) * tileHeight;
-      for (var j = 4 - i; j > 0; j--) 
-      {
-        var x = 500 - j * tileWidth;
-        var tile = new GroundTile(new Vector2(x, y), renderer);
-        _groundTiles.Add(tile);
-      }
+        _player?.Dispose();
+
+        foreach (var platform in _platforms)
+        {
+            platform.Dispose();
+        }
+
+        _platforms.Clear();
+
+        _background?.Dispose();
     }
-  }
+
+    public Player? Player => _player;
+
+    // Enum to identify which platform texture to use
+    private enum PlatformType
+    {
+        Ground,
+        Platform1,
+        Platform2,
+        Platform3,
+        Platform4
+    }
+
+    // Helper to load a platform texture with fallback to ground
+    private void LoadPlatformTexture(string contentRoot, string filename, ref string? texturePath, ref Vector2i textureSize)
+    {
+        var path = AssetHelper.GetAssetPath(contentRoot, filename);
+        if (System.IO.File.Exists(path))
+        {
+            texturePath = path;
+            textureSize = AssetHelper.GetTextureSize(path);
+        }
+        else
+        {
+            // Fallback to ground texture if not found
+            texturePath = _groundTexturePath;
+            textureSize = _groundTextureSize;
+        }
+    }
+
+    // Get the width of a platform type
+    private float GetPlatformWidth(PlatformType type)
+    {
+        return type switch
+        {
+            PlatformType.Ground => _groundTextureSize.X,
+            PlatformType.Platform1 => _platform1TextureSize.X,
+            PlatformType.Platform2 => _platform2TextureSize.X,
+            PlatformType.Platform3 => _platform3TextureSize.X,
+            PlatformType.Platform4 => _platform4TextureSize.X,
+            _ => _groundTextureSize.X
+        };
+    }
+
+    // Get the height of a platform type
+    private float GetPlatformHeight(PlatformType type)
+    {
+        return type switch
+        {
+            PlatformType.Ground => _groundTextureSize.Y,
+            PlatformType.Platform1 => _platform1TextureSize.Y,
+            PlatformType.Platform2 => _platform2TextureSize.Y,
+            PlatformType.Platform3 => _platform3TextureSize.Y,
+            PlatformType.Platform4 => _platform4TextureSize.Y,
+            _ => _groundTextureSize.Y
+        };
+    }
+
+    private void BuildLevel()
+    {
+        foreach (var platform in _platforms)
+        {
+            platform.Dispose();
+        }
+        _platforms.Clear();
+
+        if (_groundTexturePath is null || _groundTextureSize == Vector2i.Zero)
+        {
+            return;
+        }
+
+        var tileWidth = _groundTextureSize.X;
+        var tileHeight = _groundTextureSize.Y;
+
+        if (tileWidth <= 0 || tileHeight <= 0)
+        {
+            return;
+        }
+
+        // GROUND FLOOR - Use ground texture
+        var groundTilesNeeded = (int)Math.Ceiling(_viewportSize.X / (float)tileWidth) + 1;
+        for (var i = 0; i < groundTilesNeeded; i++)
+        {
+            var x = i * tileWidth + tileWidth / 2f;
+            var position = new Vector2(x, tileHeight / 2f);
+            _platforms.Add(CreateGroundTile(position));  // Use ground texture
+        }
+
+        // FLOATING PLATFORMS - Use different platform textures
+
+        // Low platform on the left - Mixed textures
+        CreateMixedHorizontalPlatform(100f, tileHeight * 3f, new[] { PlatformType.Platform1, PlatformType.Platform2, PlatformType.Platform3 });
+
+        // Mid-level platform in center - Pattern example
+        CreateMixedHorizontalPlatform(_viewportSize.X / 2f - GetPlatformWidth(PlatformType.Platform2), tileHeight * 5f,
+            new[] { PlatformType.Platform1, PlatformType.Platform2, PlatformType.Platform1, PlatformType.Platform3, PlatformType.Platform2 });
+
+        // High platform on the right - Alternating pattern
+        CreateMixedHorizontalPlatform(_viewportSize.X - 200f, tileHeight * 7f, new[] { PlatformType.Platform4, PlatformType.Platform3, PlatformType.Platform4 });
+
+        // Small stepping stones going up on left side - Each one different
+        CreateMixedHorizontalPlatform(150f, tileHeight * 6f, new[] { PlatformType.Platform1, PlatformType.Platform2 });
+        CreateMixedHorizontalPlatform(250f, tileHeight * 8f, new[] { PlatformType.Platform3, PlatformType.Platform4 });
+        CreateMixedHorizontalPlatform(350f, tileHeight * 10f, new[] { PlatformType.Platform2, PlatformType.Platform1 });
+
+        // Floating island in upper middle - Rainbow pattern
+        CreateMixedHorizontalPlatform(_viewportSize.X / 2f, tileHeight * 9f,
+            new[] { PlatformType.Platform1, PlatformType.Platform2, PlatformType.Platform3, PlatformType.Platform4, PlatformType.Platform1 });
+
+        // Small scattered platforms for challenge
+        _platforms.Add(CreatePlatformTile(new Vector2(400f, tileHeight * 4.5f), PlatformType.Platform2));
+        _platforms.Add(CreatePlatformTile(new Vector2(_viewportSize.X - 300f, tileHeight * 6.5f), PlatformType.Platform3));
+    }
+
+    // Create a tile with ground texture (for bottom floor)
+    private GroundTile CreateGroundTile(Vector2 position)
+    {
+        var renderer = new SpriteRenderer();
+        renderer.LoadAnimation("Static", _groundTexturePath!, _groundTextureSize.X, _groundTextureSize.Y, frameDurationSeconds: 0, loop: true);
+        renderer.SetAnimation("Static", true);
+        return new GroundTile(position, renderer);
+    }
+
+    // Create a tile with specified platform texture
+    private GroundTile CreatePlatformTile(Vector2 position, PlatformType type)
+    {
+        string? texturePath = null;
+        Vector2i textureSize = Vector2i.Zero;
+
+        switch (type)
+        {
+            case PlatformType.Ground:
+                texturePath = _groundTexturePath;
+                textureSize = _groundTextureSize;
+                break;
+            case PlatformType.Platform1:
+                texturePath = _platform1TexturePath;
+                textureSize = _platform1TextureSize;
+                break;
+            case PlatformType.Platform2:
+                texturePath = _platform2TexturePath;
+                textureSize = _platform2TextureSize;
+                break;
+            case PlatformType.Platform3:
+                texturePath = _platform3TexturePath;
+                textureSize = _platform3TextureSize;
+                break;
+            case PlatformType.Platform4:
+                texturePath = _platform4TexturePath;
+                textureSize = _platform4TextureSize;
+                break;
+        }
+
+        if (texturePath == null)
+        {
+            // Fallback to ground
+            texturePath = _groundTexturePath;
+            textureSize = _groundTextureSize;
+        }
+
+        var renderer = new SpriteRenderer();
+        renderer.LoadAnimation("Static", texturePath!, textureSize.X, textureSize.Y, frameDurationSeconds: 0, loop: true);
+        renderer.SetAnimation("Static", true);
+        return new GroundTile(position, renderer);
+    }
+
+    private void CreateHorizontalPlatform(float startX, float y, int length, PlatformType type)
+    {
+        var tileWidth = GetPlatformWidth(type);
+        for (var i = 0; i < length; i++)
+        {
+            var x = startX + i * tileWidth;
+            _platforms.Add(CreatePlatformTile(new Vector2(x, y), type));
+        }
+    }
+
+    // NEW: Create a horizontal platform with mixed textures
+    private void CreateMixedHorizontalPlatform(float startX, float y, PlatformType[] types)
+    {
+        var currentX = startX;
+        for (var i = 0; i < types.Length; i++)
+        {
+            var type = types[i];
+            _platforms.Add(CreatePlatformTile(new Vector2(currentX, y), type));
+            currentX += GetPlatformWidth(type); // Move by this tile's width
+        }
+    }
+
+    private void CreateVerticalPlatform(float x, float startY, int height, PlatformType type)
+    {
+        var tileHeight = GetPlatformHeight(type);
+        for (var i = 0; i < height; i++)
+        {
+            var y = startY + i * tileHeight;
+            _platforms.Add(CreatePlatformTile(new Vector2(x, y), type));
+        }
+    }
+
+    // NEW: Create a vertical platform with mixed textures
+    private void CreateMixedVerticalPlatform(float x, float startY, PlatformType[] types)
+    {
+        var currentY = startY;
+        for (var i = 0; i < types.Length; i++)
+        {
+            var type = types[i];
+            _platforms.Add(CreatePlatformTile(new Vector2(x, currentY), type));
+            currentY += GetPlatformHeight(type); // Move by this tile's height
+        }
+    }
 }
